3973a19f017a87e48571e0e8b41f493981b4c7a8
Solomon Ajayi <temmyjay001@gmail.com>
2020-06-08 13:57:42 +0100

    Update remove.js

--- routes/remove.js
+++ routes/remove.js
@@ -1,24 +1,17 @@
 const Thing = require("../models/thing");
 
 
-exports.remove = (req,res,next) => {
-    Thing.findOne({_id: req.params.id}).then(
-        (thing) => {
-            fs.unlink('images/'+ filename, () => {
-                Thing.deleteOne({_id: req.params.id}).then(
-                    () => {
-                        res.status(200).json({
-                            message:"Deleted successfully!"
-                        })
-                    
-                }).catch(
-                    (error) =>{
-                        res.status(400).json({
-                            error:error
-                        });
-                    }
-                );
-            });
-        }
-    );  
+exports.remove = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        status: true,
+        message: "File Deleted",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 };
