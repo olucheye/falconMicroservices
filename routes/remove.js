69b0dd4dfc978398bf2e51cc4b67b1e2ad41494b
Solomon Ajayi <temmyjay001@gmail.com>
2020-06-08 17:11:26 +0100

    update remove.js

--- routes/remove.js
+++ routes/remove.js
@@ -1,16 +1,6 @@
-// const Thing = require("../models/thing");
-// const {Storage} = require('@google-cloud/storage');
-
-// Creates a client
-// const storage = new Storage();
-
-exports.remove = async (req, res, next) => {
-//   try{
-  // Deletes the file from the bucket
-//   await storage.bucket('name_of_bucket').file("name_of_file").delete();
-
-//   console.log(`gs://${'name_of_bucket'}/${"name_of_file"} deleted.`);
-  model.deleteOne({ _id: req.params.id })
+const File = require("../models/file.model");
+exports.remove = (req, res, next) => {
+  File.deleteOne({ _id: req.params.id })
     .then(() => {
       res.status(200).json({
         status: true,
@@ -19,10 +9,7 @@ exports.remove = async (req, res, next) => {
     })
     .catch((error) => {
       res.status(400).json({
-        error: error,
+        status: error,
       });
     });
-//   } catch(error){
-//     console.log(error);
-//   }
 };
